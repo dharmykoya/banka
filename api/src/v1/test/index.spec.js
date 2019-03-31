19c3c8fd79af408f9930627530a0c1b4d02f4739
Damilola Adekoya <dharmykoya38@gmail.com>
2019-04-01 00:46:24 +0100

    testing for the app entry point

---
+++ api/src/v1/test/index.spec.js
@@ -0,0 +1,20 @@
+/* eslint-env mocha */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../index';
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('Test for the App entry point', () => {
+  it('Should return Welcome message', () => {
+    chai
+      .request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equal('Welcome to Banka by Damilola Adekoya By Damilola Adekoya');
+      });
+  });
+});
