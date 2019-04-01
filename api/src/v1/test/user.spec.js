08ae832f1c6a3f4432c793f6b366aa654a693245
Damilola Adekoya <dharmykoya38@gmail.com>
2019-04-01 01:23:02 +0100

    test for user can register

---
+++ api/src/v1/test/user.spec.js
@@ -0,0 +1,35 @@
+/* eslint-env mocha */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../index';
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('The authentication endpoint test', () => {
+  /**
+   * test for Signup endpoint
+   * to see if the res body is an object
+   */
+  describe('Signup user endpoint', () => {
+    it('should add a new user and return a token', (done) => {
+      chai
+        .request(app)
+        .post('/api/v1/auth/signup')
+        .send({
+          email: 'dami.gmail.com',
+          firstName: 'damilola',
+          lastName: 'Koya',
+          password: 'bankappclient',
+          type: 'client',
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(201);
+          expect(res.body.status).to.be.equal(201);
+          expect(res.body.data).to.have.key('id', 'token', 'email', 'firstName', 'lastName', 'password', 'type');
+          done();
+        });
+    });
+  });
+});
