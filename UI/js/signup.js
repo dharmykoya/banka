21fd17d8ff0f2f96c769bd2d8a767765f681e711
Damilola Adekoya <dharmykoya38@gmail.com>
2019-04-08 18:17:59 +0100

    bug(javascript validation):javascript validation adding javascript validation to all my forms [Start #165170147]

---
+++ UI/js/signup.js
@@ -0,0 +1,24 @@
+  
+  const form = document.querySelector('#signupForm');
+  const message = document.querySelector('.message');
+  const alert = document.querySelector('.alert');
+  const validate = () => {
+
+    if(form.email.value == "" ) {
+      message.innerText = "Please provide your email!";
+      alert.style.display = 'block';
+      form.email.focus() ;
+      return false;
+    }
+    if( form.password.value == "" ) {
+      message.innerText = "Please provide your password!";
+      alert.style.display = 'block';
+      form.password.focus() ;
+      return false;
+    }
+  }
+
+  const errorAlert = () => {
+    const span = document.querySelector('.closebtn');
+    span.parentElement.style.display = 'none';
+  }
