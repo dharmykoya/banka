985800b752e87cde22cb6d8b43d71356308de52f
Damilola Adekoya <dharmykoya38@gmail.com>
2019-04-28 09:01:14 +0100

    bug(picture-upload):picture upload Sepearting the picture upload methods Moving the picture upload methods to a seperate file [Delivers #165655601]

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,41 +1,42 @@
 import { Router } from 'express';
-import multer from 'multer';
+// import multer from 'multer';
+import Upload from '../services/multer';
 import UserController from '../controllers/user.controller';
 import Auth from '../middleware/Auth';
 
 const router = Router();
 
-const storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, 'uploads/');
-  },
-  filename(req, file, callback) {
-    callback(null, new Date().toISOString().replace(/:/g, '-')
-    + file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination(req, file, callback) {
+//     callback(null, 'uploads/');
+//   },
+//   filename(req, file, callback) {
+//     callback(null, new Date().toISOString().replace(/:/g, '-')
+//     + file.originalname);
+//   },
+// });
 
-const fileFilter = async (req, file, callback) => {
-  try {
-    if (file.mimetype === 'image/jpeg'
-        || file.mimetype === 'image/jpg'
-        || file.mimetype === 'image/png') {
-      callback(null, true);
-    } else {
-      return await callback(new Error('upload the right format please'), false);
-    }
-  } catch (err) {
-    return err;
-  }
-  return true;
-};
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-  },
-  fileFilter,
-});
+// const fileFilter = async (req, file, callback) => {
+//   try {
+//     if (file.mimetype === 'image/jpeg'
+//         || file.mimetype === 'image/jpg'
+//         || file.mimetype === 'image/png') {
+//       callback(null, true);
+//     } else {
+//       return await callback(new Error('upload the right format please'), false);
+//     }
+//   } catch (err) {
+//     return err;
+//   }
+//   return true;
+// };
+// const upload = multer({
+//   storage,
+//   limits: {
+//     fileSize: 1024 * 1024 * 5,
+//   },
+//   fileFilter,
+// });
 
 // returns all the accounts attached to a user
 router.get('/:email/accounts',
@@ -44,7 +45,7 @@ router.get('/:email/accounts',
 
 // route to upload a picture
 router.patch('/upload',
-  upload.single('profileImage'),
+  Upload.single('profileImage'),
   Auth.getUser,
   UserController.uploadPicture);
 
