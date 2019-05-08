c62e0989a1cdb4dd790eb3b5108f451cd3cce610
Damilola Adekoya <dharmykoya38@gmail.com>
2019-05-09 00:23:29 +0100

    feature(connect-adminDashboard):admin dashboard Connect admin dashboard Connect admin dashboard to backend API [Deliver #165859106]

---
+++ frontend/js/adminDashboard.js
@@ -0,0 +1,16 @@
+const token = sessionStorage.getItem('token');
+const email = sessionStorage.getItem('email');
+const userId = sessionStorage.getItem('id');
+const firstName = sessionStorage.getItem('firstName');
+const lastName = sessionStorage.getItem('lastName');
+const adminName = document.querySelector('#admin-name');
+const adminEmail = document.querySelector('#admin-email');
+const adminId = document.querySelector('#admin-Id');
+if (!token) {
+  window.location.replace('./signin.html');
+}
+window.onload = () => {
+  adminName.textContent = `${firstName} ${lastName}`;
+  adminEmail.textContent = email;
+  adminId.textContent = userId;
+};
